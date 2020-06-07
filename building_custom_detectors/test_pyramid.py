ffb89b1d161ada66208ffae590e6a9a76096aa59
dat-adi <naruita201@gmail.com>
2020-06-07 23:18:56 +0530

    Apparently, it's used in a different function as a process lol.

--- building_custom_detectors/test_pyramid.py
+++ building_custom_detectors/test_pyramid.py
@@ -2,13 +2,18 @@ import argparse
 import cv2
 import imutils
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the input image")
+ap.add_argument("-s", "--scale", type=float, default=1.5, help="scale factor size")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
 
 def pyramid(image, scale=1.5, min_size=(30, 30)):
     yield image
     while True:
         w = int(image.shape[1]/scale)
         image = imutils.resize(image, width=w)
-        print('3')
         if image.shape[0] < min_size[1] or image.shape[1] < min_size[0]:
             break
         yield image
@@ -16,14 +21,4 @@ def pyramid(image, scale=1.5, min_size=(30, 30)):
         for(i, layer) in enumerate(pyramid(image, scale=args["scale"])):
             cv2.imshow("Layer {}".format(i+1), layer)
 
-        cv2.waitKey(0)
-
-
-if __name__ == "__main__":
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True, help="Path to the input image")
-    ap.add_argument("-s", "--scale", type=float, default=1.5, help="scale factor size")
-    args = vars(ap.parse_args())
-    image = cv2.imread(args["image"])
-
-    pyramid(image)
+    cv2.waitKey(0)
--- building_custom_detectors/test_sliding_windows.py
+++ building_custom_detectors/test_sliding_windows.py
@@ -1,26 +1,27 @@
 import argparse
 import time
 import cv2
+from test_pyramid import pyramid
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to input image")
-ap.add_argument("-w", "--width", type=int, help="Width to sliding windows")
-ap.add_argument("-t", "--height", type=int, help="height to sliding windows")
+ap.add_argument("-w", "--width", type=int, default=300, help="Width to sliding windows")
+ap.add_argument("-t", "--height", type=int, default=300, help="height to sliding windows")
 ap.add_argument("-s", "--scale", type=float, default=1.5, help="scale factor size")
 args = vars(ap.parse_args())
 
 
-def sliding_windows(image, stepSize, windowSize):
-    for y in range(0, image.shape[0], stepSize):
-        for x in range(0, image.shape[1], stepSize):
-            yield(x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+def sliding_windows(img, step_size, window_size):
+    for y in range(0, img.shape[0], step_size):
+        for x in range(0, img.shape[1], step_size):
+            yield x, y, img[y:y + window_size[1], x:x + window_size[0]]
 
 
 image = cv2.imread(args["image"])
 (winW, winH) = (args["width"], args["height"])
 
 for layer in pyramid(image, scale=args["scale"]):
-    for(x, y, window) in sliding_windows(layer, stepSize=32, windowSize=(winW, winH)):
+    for(x, y, window) in sliding_windows(layer, step_size=32, window_size=(winW, winH)):
         if window.shape[0] != winH or window.shape[1] != winW:
             continue
 

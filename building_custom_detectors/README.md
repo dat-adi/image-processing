5bd6160997d134a38d3508ad7f985905216ae809
dat-adi <naruita201@gmail.com>
2020-06-07 22:28:59 +0530

    the function doesn't seem to work?

--- building_custom_detectors/README.md
+++ building_custom_detectors/README.md
@@ -1,7 +1,7 @@
 This folder is used to check on the programs which work at this point in time.<br>
 
 - [ ] train_detector
-- [ ] test_detector
+- [x] test_detector
 - [ ] test_pyramid
 - [ ] test_sliding_windows
 - [ ] train_detector_1
--- building_custom_detectors/test_pyramid.py
+++ building_custom_detectors/test_pyramid.py
@@ -2,27 +2,28 @@ import argparse
 import cv2
 import imutils
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the input image")
-ap.add_argument("-s", "--scale", type=float, default=1.5, help="scale factor size")
-args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
-
-
-def pyramid(image, scale=1.5, minSize=(30, 30)):
+def pyramid(image, scale=1.5, min_size=(30, 30)):
     yield image
-
     while True:
         w = int(image.shape[1]/scale)
         image = imutils.resize(image, width=w)
-
-        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+        print('3')
+        if image.shape[0] < min_size[1] or image.shape[1] < min_size[0]:
             break
-    yield image
+        yield image
 
-    for(i, layer) in enumerate(pyramid(image, scale=args["scale"])):
-        cv2.imshow("Layer {}".format(i+1), layer)
+        for(i, layer) in enumerate(pyramid(image, scale=args["scale"])):
+            cv2.imshow("Layer {}".format(i+1), layer)
+
+        cv2.waitKey(0)
 
 
-cv2.waitKey(0)
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the input image")
+    ap.add_argument("-s", "--scale", type=float, default=1.5, help="scale factor size")
+    args = vars(ap.parse_args())
+    image = cv2.imread(args["image"])
+
+    pyramid(image)

d55454c597b8a6b4e5e522c33cae52861b219d64
dat-adi <naruita201@gmail.com>
2020-06-14 22:21:20 +0530

    hull contour implementation

--- testing/detect_carplate.py
+++ testing/detect_carplate.py
@@ -23,7 +23,7 @@ cv2.waitKey(0)
 blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 cv2.imshow("image", image)
 
-(T, threshInv) = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+(T, threshInv) = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 cv2.imshow("Threshold", threshInv)
 print("Otsu's threshold value : {}".format(T))
 
@@ -38,13 +38,23 @@ cv2.imshow("Auto Canny Result", auto)
 cv2.waitKey(0)
 
 (cnts, _) = cv2.findContours(auto.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-clone = image.copy()
+clone = gray.copy()
 
 cv2.drawContours(clone, cnts, -1, (0, 255, 0), 2)
 print("Found {} contours".format(len(cnts)))
+hull = []
+
+for i in range(len(cnts)):
+    hull.append(cv2.convexHull(cnts[i], False))
 
 for(i, c) in enumerate(cnts):
     print("Drawing contour #{}".format(i + 1))
-    cv2.drawContours(clone, [c], -1, (0, 255, 0), 1)
-    cv2.imshow("All Contours", clone)
+    cv2.drawContours(clone, [c], -1, (0, 255, 0), 2)
+    cv2.imshow("Normal Contours", clone)
+cv2.waitKey(0)
+
+for(i, c) in enumerate(cnts):
+    print("Drawing contour #{}".format(i + 1))
+    cv2.drawContours(clone, hull, i, (0, 255, 0), 2)
+    cv2.imshow("Hull Contours", clone)
 cv2.waitKey(0)

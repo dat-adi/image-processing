3ef30c200dd0a5cb4afff21e55e59b4e9bfcecfc
dat-adi <naruita201@gmail.com>
2020-06-14 21:27:04 +0530

    Contour implementation

--- testing/detect_carplate.py
+++ testing/detect_carplate.py
@@ -20,10 +20,6 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("GrayScale", gray)
 cv2.waitKey(0)
 
-(cnts, _) = cv2.findContours(gray.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-clone = image.copy()
-cv2.drawContours(clone, cnts, -1, (0, 255, 0), 2)
-print("Found {} contours".format(len(cnts)))
 blurred = cv2.GaussianBlur(gray, (7, 7), 0)
 cv2.imshow("image", image)
 
@@ -39,4 +35,16 @@ tight = cv2.Canny(otsu_output, 225, 250)
 auto = auto_canny(otsu_output)
 cv2.imshow("Tight Canny", tight)
 cv2.imshow("Auto Canny Result", auto)
+cv2.waitKey(0)
+
+(cnts, _) = cv2.findContours(auto.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+clone = image.copy()
+
+cv2.drawContours(clone, cnts, -1, (0, 255, 0), 2)
+print("Found {} contours".format(len(cnts)))
+
+for(i, c) in enumerate(cnts):
+    print("Drawing contour #{}".format(i + 1))
+    cv2.drawContours(clone, [c], -1, (0, 255, 0), 1)
+    cv2.imshow("All Contours", clone)
 cv2.waitKey(0)

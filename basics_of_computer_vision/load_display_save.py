70e7e73f82d7237a265680a75a20608e4e159731
dat-adi <dat.adithya@gmail.com>
2021-02-22 12:42:22 +0530

    Modified load_display_saves

--- basics_of_computer_vision/load_display_save.py
+++ basics_of_computer_vision/load_display_save.py
@@ -5,19 +5,38 @@ Created on Wed Oct 16 21:41:25 2019
 @author: narui
 """
 
+# importing argument parsers
 import argparse
+
+# importing the OpenCV module
 import cv2
 
+
+# initializing an argument parser object
 ap = argparse.ArgumentParser()
+
+# adding the argument, providing the user an option
+# to input the path of the image
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
+
+# parsing the argument
 args = vars(ap.parse_args())
 
+# reading the image location through args
+# and reading the image using cv2.imread
 image = cv2.imread(args["image"])
+
+# printing out the various dimensions of the image
 print("width : ", image.shape[1])
 print("height : ", image.shape[0])
 print("channels : ", image.shape[2])
 
+# Displays the image using cv2.imshow
 cv2.imshow("Image", image)
+
+# Displays the image until the user presses a key closing the display
 cv2.waitKey(0)
 
+# Writing the image into the same folder as the script
+# But under a new name
 cv2.imwrite("newimage.jpg", image)
--- basics_of_computer_vision/load_display_save1.py
+++ basics_of_computer_vision/load_display_save1.py
@@ -1,11 +1,19 @@
-import cv2  # imports the OpenCV functions into the program.
+# importing OpenCV into the script.
+import cv2
 
+# Reading the image location using a hardcoded path
+# and using cv2.imread to read the image
 image = cv2.imread(
     "C:\\Users\\narui\\PycharmProjects\\image-processing\\assets\\ey.jpg"
 )
+
+# printing out the dimensions of the image
 print("width: ", image.shape[1])
 print("height: ", image.shape[0])
 print("channels: ", image.shape[2])
 
+# Displaying the image using the cv2.imshow method
 cv2.imshow("Image", image)
+
+# Displays the image until the user presses a key closing the display
 cv2.waitKey(0)

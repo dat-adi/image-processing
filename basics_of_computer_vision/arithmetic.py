f6d403a20e213c0b6e36abbea264697f4d6131fc
dat-adi <dat.adithya@gmail.com>
2021-02-26 17:53:02 +0530

    Updated arithmetic, bitwise and masking with documentation

--- basics_of_computer_vision/arithmetic.py
+++ basics_of_computer_vision/arithmetic.py
@@ -1,25 +1,41 @@
+# importing numpy to work with pixels
 import numpy as np
-import cv2
+
+# importing argument parsers
 import argparse
 
+# importing the OpenCV module
+import cv2
+
+
+# initializing an argument parser object
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the Image")
+
+# adding the argument, providing the user an option
+# to input the path of the image
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+
+# parsing the argument
 args = vars(ap.parse_args())
 
+# reading the image location through args
+# and reading the image using cv2.imread
 image = cv2.imread(args["image"])
-cv2.imshow("Original", image)
 
+# printing out details of image min, max and the wrap around
 print("max of 255 :", str(cv2.add(np.uint8([200]), np.uint8([100]))))
 print("min of 0 :", str(cv2.subtract(np.uint8([50]), np.uint8([100]))))
 
 print("wrap around :", str(np.uint8([200]) + np.uint8([100])))
 print("wrap around :", str(np.uint8([50]) - np.uint8([100])))
 
+# adding pixels of value 255 (white) to the image
 M = np.ones(image.shape, dtype="uint8") * 100
 added = cv2.add(image, M)
 cv2.imshow("Added", added)
 cv2.waitKey(0)
 
+# adding pixels of value 0 (black) to the image
 M = np.ones(image.shape, dtype="uint8") * 50
 subtracted = cv2.subtract(image, M)
 cv2.imshow("Subtracted", subtracted)
--- basics_of_computer_vision/masking.py
+++ basics_of_computer_vision/masking.py
@@ -1,25 +1,52 @@
+# importing numpy to work with pixels
 import numpy as np
+
+# importing argument parsers
 import argparse
+
+# importing the OpenCV module
 import cv2
 
+
+# initializing an argument parser object
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image.")
+
+# adding the argument, providing the user an option
+# to input the path of the image
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+
+# parsing the argument
 args = vars(ap.parse_args())
 
+# reading the image location through args
+# and reading the image using cv2.imread
 image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
+# creating a mask of that has the same dimensions of the image
+# where each pixel is valued at 0
 mask = np.zeros(image.shape[:2], dtype="uint8")
+
+# creating a rectangle on the mask
+# where the pixels are valued at 255
 cv2.rectangle(mask, (0, 90), (290, 450), 255, -1)
 cv2.imshow("Mask", mask)
 
+# performing a bitwise_and with the image and the mask
 masked = cv2.bitwise_and(image, image, mask=mask)
 cv2.imshow("Mask applied to Image", masked)
 cv2.waitKey(0)
 
+# creating a mask of that has the same dimensions of the image
+# where each pixel is valued at 0
 mask = np.zeros(image.shape[:2], dtype="uint8")
+
+# creating a rectangle on the mask
+# where the pixels are valued at 255
 cv2.circle(mask, (145, 200), 100, 255, -1)
-masked = cv2.bitwise_and(image, image, mask=mask)
 cv2.imshow("Mask", mask)
+
+# performing a bitwise_and with the image and the mask
+masked = cv2.bitwise_and(image, image, mask=mask)
 cv2.imshow("Mask applied to Image", masked)
 cv2.waitKey(0)

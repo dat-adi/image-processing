658e67d39b480e2cb51918ca068d3944c962d8bd
dat-adi <dat.adithya@gmail.com>
2021-03-05 00:34:07 +0530

    Basic import documentation

--- basics_of_computer_vision/splitting_and_merging.py
+++ basics_of_computer_vision/splitting_and_merging.py
@@ -1,11 +1,25 @@
+# importing numpy to work with pixels
 import numpy as np
+
+# importing argument parsers
 import argparse
+
+# importing the OpenCV module
 import cv2
 
+
+# initializing an argument parser object
 ap = argparse.ArgumentParser()
+
+# adding the argument, providing the user an option
+# to input the path of the image
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
+
+# parsing the argument
 args = vars(ap.parse_args())
 
+# reading the image location through args
+# and reading the image using cv2.imread
 image = cv2.imread(args["image"])
 (B, G, R) = cv2.split(image)
 

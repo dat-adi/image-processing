fbf4af74450cb70a20065d13085675b072b8c2a1
dat-adi <dat.adithya@gmail.com>
2021-03-16 20:07:26 +0530

    Documented bilateral.py

--- basics_of_computer_vision/bilateral.py
+++ basics_of_computer_vision/bilateral.py
@@ -1,14 +1,28 @@
-import cv2
+# importing argument parsers
 import argparse
 
+# importing the OpenCV module
+import cv2
+
+# initializing an argument parser object
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the Image")
+
+# adding the argument, providing the user an option
+# to input the path of the image
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+
+# parsing the argument
 args = vars(ap.parse_args())
 
+# reading the image location through args
+# and reading the image using cv2.imread
 image = cv2.imread(args["image"])
+
+# Displaying the original image
 cv2.imshow("Original", image)
 params = [(11, 21, 7), (11, 41, 21), (11, 61, 39)]
 
+# blurring images and reducing noise while preserving edges
 for (diameter, sigmaColor, sigmaSpace) in params:
     blurred = cv2.bilateralFilter(image, diameter, sigmaColor, sigmaSpace)
     title = "Blurred d = {}, sc = {}, ss = {}".format(diameter, sigmaColor, sigmaSpace)
